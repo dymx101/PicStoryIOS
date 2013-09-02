262eec9a7474be981eb31aad3eddbf55750f99f4
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-09-02 14:38:37 +0800

    add nyxImagesKit

--- PicStoryIOS/PicStoryIOS/TMDViewController.m
+++ PicStoryIOS/PicStoryIOS/TMDViewController.m
@@ -10,6 +10,7 @@
 #import "TMDLayerAnimation.h"
 
 #import "TMDStoryTestVC.h"
+#import "NYXImagesKit.h"
 
 @interface TMDViewController ()
 
@@ -36,13 +37,17 @@
     [TMDLayerAnimation pulse:view.layer];
     [self.view addSubview:view];
     
+    /////////////////////////
     
+    
+    //////////////////////
     
     [CATransaction setDisableActions:NO];
     _layer = [CALayer layer];
-    _layer.bounds = CGRectMake(0, 0, 100, 100);
+    
+    _layer.bounds = CGRectMake(0, 0, 300, 200);
     _layer.position = CGPointMake(160, 250);
-    _layer.backgroundColor = [UIColor blackColor].CGColor;
+    _layer.backgroundColor = [UIColor blueColor].CGColor;
     _layer.borderColor = [UIColor redColor].CGColor;
     _layer.opacity = 1.0f;
     
@@ -51,10 +56,34 @@
     
 }
 
+-(void)testImageKit
+{
+    UIImage *image = [UIImage imageNamed:@"sample.jpg"];
+    
+    
+    //UIImage* masked = [image maskWithImage:[UIImage imageNamed:@"mask.jpg"]];
+    
+    UIImage* cropped = [image cropToSize:(CGSize){100, 100} usingMode:NYXCropModeCenter];
+    
+    //UIImage *blurImg = [cropped gaussianBlurWithBias:100];
+    
+    //    UIImage* rotated1 = [cropped rotateInDegrees:217.0f];
+    //    UIImage* rotated2 = [cropped rotateInRadians:M_PI_2];
+    //    UIImage* flipped1 = [cropped verticalFlip];
+    //    UIImage* flipped2 = [cropped horizontalFlip];
+    
+    //UIImage* reflected = [cropped reflectedImageWithHeight:cropped.size.height fromAlpha:0.0f toAlpha:0.5f];
+    
+    //UIImage *enhanced = [cropped autoEnhance];
+    //UIImage *redeyecorrection = [cropped redEyeCorrection];
+    
+    _layer.contents = (id)cropped.CGImage;
+}
+
 -(void)testImplicitLayerAnimation
 {
    // _layer.position = CGPointMake(100, 200);
-    _layer.opacity = .2f;
+    //_layer.opacity = .2f;
 }
 
 -(IBAction)startStoryAction:(id)sender
