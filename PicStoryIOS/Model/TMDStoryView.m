6e65ab31ca9a2648dcb0a9840626089d8b4b51f4
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-09-10 15:05:49 +0800

    emit snow

--- PicStoryIOS/Model/TMDStoryView.m
+++ PicStoryIOS/Model/TMDStoryView.m
@@ -42,6 +42,8 @@
     self.backgroundColor = [UIColor clearColor];
     self.clipsToBounds = YES;
     self.layer.masksToBounds = YES;
+    
+    [self emitSnows];
 }
 
 
@@ -66,6 +68,44 @@
 -(void)addLayer:(CALayer *)aLayer
 {
     [self.layer addSublayer:aLayer];
+}
+
+-(void)emitSnows
+{
+    // Configure the particle emitter to the top edge of the screen
+	CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
+    snowEmitter.zPosition = 1000;
+	snowEmitter.emitterPosition = CGPointMake(self.bounds.size.width / 2.0, -30);
+	snowEmitter.emitterSize		= CGSizeMake(self.bounds.size.width * 2.0, 0.0);;
+	
+	// Spawn points for the flakes are within on the outline of the line
+	snowEmitter.emitterMode		= kCAEmitterLayerOutline;
+	snowEmitter.emitterShape	= kCAEmitterLayerLine;
+	
+	// Configure the snowflake emitter cell
+	CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
+	
+	snowflake.birthRate		= 1.0;
+	snowflake.lifetime		= 120.0;
+	
+	snowflake.velocity		= -10;				// falling down slowly
+	snowflake.velocityRange = 10;
+	snowflake.yAcceleration = 2;
+	snowflake.emissionRange = 0.5 * M_PI;		// some variation in angle
+	snowflake.spinRange		= 0.25 * M_PI;		// slow spin
+	
+	snowflake.contents		= (id) [[UIImage imageNamed:@"DazFlake"] CGImage];
+	snowflake.color			= [[UIColor colorWithRed:0.600 green:0.658 blue:0.743 alpha:1.000] CGColor];
+    
+	// Make the flakes seem inset in the background
+	snowEmitter.shadowOpacity = 1.0;
+	snowEmitter.shadowRadius  = 0.0;
+	snowEmitter.shadowOffset  = CGSizeMake(0.0, 1.0);
+	snowEmitter.shadowColor   = [[UIColor whiteColor] CGColor];
+	
+	// Add everything to our backing layer below the UIContol defined in the storyboard
+	snowEmitter.emitterCells = [NSArray arrayWithObject:snowflake];
+	[self.layer insertSublayer:snowEmitter atIndex:0];
 }
 
 @end
